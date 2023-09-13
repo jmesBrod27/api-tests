729ab790e1a7a15db2e0a8c140a58431ea6d9c8f
jamesbroderick <james772772@gmail.com>
2023-09-13 13:18:16 +0100

    changes

---
+++ src/test/java/apiTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+public class apiTest {
+
+    @Test
+    public void
+    lotto_resource_returns_200_with_expected_id_and_winners() {
+
+        when().
+                get("/lotto/{id}", 5).
+                then().
+                statusCode(200).
+                body("lotto.lottoId", equalTo(5),
+                        "lotto.winners.winnerId", hasItems(23, 54));
+
+    }
+}
